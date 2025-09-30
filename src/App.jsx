3368d5f49aa27d0eb6ff77234137bfc37f5067b3
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-09-30 15:05:00 +0100

    enabled environment variables in vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 function App() {
+  const test = null;
+
   return <div>SPOTIFY NOTES</div>;
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    eslint(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+      eslint(),
+    ],
+    define: {
+      'process.env': env,
+    },
+  };
 });
