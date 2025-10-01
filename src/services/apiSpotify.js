28228ec100054063c895071f09c21ce6c67b0527
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-10-01 16:37:59 +0100

    As Spotify no longer allow localhost as a redirect uri I found a way to get Vite to run its dev server on the loopback address 127.0.0.1 - see vite.config

--- src/services/apiSpotify.js
+++ src/services/apiSpotify.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = 'ecbe55ef331142259862b25a9f54a3ea'; //import.meta.env.SPOTIFY_CLIENT_ID;
-const REDIRECT_URI = 'http://192.168.1.124:5173/';
+const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID; //'ecbe55ef331142259862b25a9f54a3ea';
+const REDIRECT_URI = 'http://127.0.0.1:5173/';
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const RESPONSE_TYPE = 'token';
 
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [
-      react({
-        babel: {
-          plugins: [['babel-plugin-react-compiler']],
-        },
-      }),
-      eslint(),
-    ],
-    // define: {
-    //   'process.env': env,
-    //   process: { env },
-    // },
-  };
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    eslint(),
+  ],
+  server: { host: '127.0.0.1', port: 5173 },
 });
