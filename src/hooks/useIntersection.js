2884fecb512d070fb44838842782565c1ac0f412
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-12-02 14:59:00 +0000

    JSDocs added to useIntersection

--- src/hooks/useIntersection.js
+++ src/hooks/useIntersection.js
@@ -1,5 +1,14 @@
 import { useCallback, useEffect, useRef } from 'react';
-
+/**
+ *
+ * @param {Function} callbackFn - function to execute on intersection with arguments of target element and observer object eg callback(target, observer)
+ * @param {Object} options - default: none - same as options object detailed here https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API except that the root is always the viewportElementRef or if not set then it is the browser viewport
+ * @param {Boolean} singleUse - default: true - whether to remove the observer after it has first triggered the callbackFn (ie when using it for infinite scroll loading you'll want it to only trigger once when it first comes into view)
+ * @typedef {Object} Refs
+ * @property {React.Ref} intersectionTargetRef - attach this to the element that you are tracking the intersection of - ie when observing a div it would be <div ref={intersectionTargetRef}></div>
+ * @property {React.Ref} viewportElementRef - defaults to the browser viewport but can be any element so it would trigger the callback when intersectionTargetRef element entered the element with this ref attached (MUST BE AN ANCESTOR OF THE TARGET ELEMENT)
+ * @returns {Refs} An object containing the refs to attach to the element to be observed and optionally the ancestor element to behave as the viewport
+ */
 export function useIntersection(callbackFn, options = {}, singleUse = true) {
   const intersectionTargetRef = useRef(null);
   const viewportElementRef = useRef(null);
@@ -36,7 +45,7 @@ export function useIntersection(callbackFn, options = {}, singleUse = true) {
       }
       observer?.disconnect();
     };
-  }, [intersectionTargetRef, observerCallback]);
+  }, [intersectionTargetRef, observerCallback, options]);
 
   return { intersectionTargetRef, viewportElementRef };
 }
