bea15513dd2fa245042dd36ae77f1ce5a67ee045
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-12-08 12:56:13 +0000

    fixed a little typo bug is usePaginatedFetch

--- src/query-hooks/usePaginatedFetch.js
+++ src/query-hooks/usePaginatedFetch.js
@@ -1,36 +1,4 @@
-// import { useState } from 'react';
-// //I've added the rest operator so that we can pass in extra arguments if needed, such as an id for using it with the playlist tracks functionality
-// export function usePaginatedFetch(fetchFunction, initialState, ...args) {
-//   const [data, setData] = useState(initialState);
-
-//   let hasNext = data?.next;
-//   if (hasNext) {
-//     hasNext = new URL(hasNext);
-//   }
-//   let hasPrevious = data?.previous;
-//   if (hasPrevious) {
-//     hasPrevious = new URL(hasPrevious);
-//   }
-//   //let's do direction as a positive number for next and negative for previous
-//   const getNextPrev = async (direction) => {
-//     if (hasNext && direction > 0) {
-//       const offset = hasNext.searchParams.get('offset') || 0;
-//       const limit = hasNext.searchParams.get('limit') || 50;
-//       await fetchFunction(offset, limit, args).then((data) => {
-//         setData(data);
-//       });
-//     }
-//     if (hasPrevious && direction < 0) {
-//       const offset = hasPrevious.searchParams.get('offset') || 0;
-//       const limit = hasPrevious.searchParams.get('limit') || 50;
-//       await fetchFunction(offset, limit, args).then((data) => {
-//         setData(data);
-//       });
-//     }
-//   };
-//   return { data, getNextPrev, hasNext, hasPrevious };
-// }
-
+//I've added the rest operator so that we can pass in extra arguments if needed, such as an id for using it with the playlist tracks functionality
 //Asked copilot for suggestion on making this use react-query infinite scroll -------
 
 import { useMemo } from 'react';
@@ -44,7 +12,7 @@ export function usePaginatedFetch(fetchFunction, initialState, ...args) {
   const limit = Number(initialState?.limit ?? 20);
   const queryKey = [fetchFunction.name || 'paginated', ...args];
 
-  let enabledCheck = true;
+  let enabledCheck;
   try {
     enabledCheck = !isTokenExpiring();
   } catch (error) {
