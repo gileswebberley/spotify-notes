e50eb8363d42cc349f7b4fcde57aac260232a819
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-12-09 16:58:16 +0000

    implemented line clamping for the playlist header name

--- src/ui/PlaylistHeaderInfo.jsx
+++ src/ui/PlaylistHeaderInfo.jsx
@@ -1,14 +1,23 @@
 import { LuDot } from 'react-icons/lu';
 import { useUser } from '../query-hooks/useUser';
 import UserImage from './UserImage';
+import { useState } from 'react';
 
 function PlaylistHeaderInfo({ name, tracks, owner }) {
   const { status, fetchStatus, user, error } = useUser(owner?.id);
   const { display_name, images } = user ?? {};
+  const [clampName, setClampName] = useState(true);
 
   return (
     <section className="playlist-header-info">
-      <h2>{name}</h2>
+      <div className="clamp-wrapper">
+        <h2
+          className={clampName ? 'clamped' : ''}
+          onClick={() => setClampName(!clampName)}
+        >
+          {name}
+        </h2>
+      </div>
       <article className="playlist-header-user">
         {status === 'pending' ? (
           'loading...'
