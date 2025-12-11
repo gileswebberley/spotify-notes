5fd22e729411a01e96572e1a45c81f380f7723cf
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-12-11 19:16:08 +0000

    vite config now ignores eslint in build

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import { HOST, PORT } from './src/utils/constants';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    eslint(),
-  ],
+    // Only run eslint in development
+    mode === 'development' && eslint(),
+  ].filter(Boolean),
   server: { host: HOST, port: PORT },
-});
+}));
