68c345a273fc30ce55cf9ad65ac522dfc54d65ea
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2026-01-16 16:47:21 +0000

    query hook for using getCurrentlyPlaying

---
+++ src/query-hooks/useCurrentlyPlaying.js.js
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCurrentlyPlayingTrack, isLoggedIn } from '../services/apiSpotify';
+
+export function useCurrentlyPlaying() {
+  let enableCheck = isLoggedIn();
+  const {
+    status,
+    fetchStatus,
+    data: currentlyPlaying,
+    error,
+  } = useQuery({
+    queryKey: ['currently_playing'],
+    queryFn: () => getCurrentlyPlayingTrack(),
+    enabled: enableCheck,
+    useErrorBoundary: true,
+    staleTime: 5000,
+  });
+
+  return { status, fetchStatus, currentlyPlaying, error };
+}
